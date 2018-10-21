991aa4b455a60d42cba48eb593eddc70d81bbd92
Alexander Chirlin <alexander.chirlin@gigya-inc.com>
2018-10-21 12:57:33 +0300

    41458, Avoid invalid argument exception, when possible, to make debugging simpler

--- Gigya.Microdot.Ninject/RegexTimeoutInitializer.cs
+++ Gigya.Microdot.Ninject/RegexTimeoutInitializer.cs
@@ -5,23 +5,26 @@ namespace Gigya.Microdot.Ninject
 {
    
     /// <summary>
-    /// Notice that REGEX_DEFAULT_MATCH_TIMEOUT can be set only once and will be determined when calling the first regex the default in infinite!!
+    /// Notice that REGEX_DEFAULT_MATCH_TIMEOUT can be set only once and will be determined when calling the first regex, the default is infinite!!
     /// </summary>
     public class RegexTimeoutInitializer 
     {
         public void Init()
         {
-            int regexDefaultMachTimeOutMs =(int) TimeSpan.FromSeconds(10).TotalMilliseconds;
+            int regexDefaultMatchTimeOutMs = (int)TimeSpan.FromSeconds(10).TotalMilliseconds;
             try
             {
-                regexDefaultMachTimeOutMs = int.Parse(ConfigurationManager.AppSettings["regexDefaultMachTimeOutMs"]);
+                var strValue = ConfigurationManager.AppSettings["regexDefaultMachTimeOutMs"];
+                if(strValue != null)
+                    regexDefaultMatchTimeOutMs = int.Parse(strValue);
             }
-            catch (Exception e)
+            catch (Exception)
             {
+                // there is nothing to do about
             }
 
-            AppDomain.CurrentDomain.SetData("REGEX_DEFAULT_MATCH_TIMEOUT",TimeSpan.FromMilliseconds(regexDefaultMachTimeOutMs));
-            Console.WriteLine($"REGEX_DEFAULT_MATCH_TIMEOUT is set to {regexDefaultMachTimeOutMs} ms");
+            AppDomain.CurrentDomain.SetData("REGEX_DEFAULT_MATCH_TIMEOUT", TimeSpan.FromMilliseconds(regexDefaultMatchTimeOutMs));
+            Console.WriteLine($"REGEX_DEFAULT_MATCH_TIMEOUT is set to {regexDefaultMatchTimeOutMs} ms");
         }
     }
 }
